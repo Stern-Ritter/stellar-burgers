1945e3ae6360c1a9509d973f6d86212338775204
Stern-Ritter <fadeew.a@mail.ru>
2022-01-24 14:41:37 +0300

    Edit api response PropTypes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,38 +1,28 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const API = 'https://norma.nomoreparties.space/api';
+const API = "https://norma.nomoreparties.space/api";
 
 function checkResponse(res, type) {
   const status = res.ok;
   const contentType = res.headers.get("content-type");
-  if(status && contentType.includes(type)) {
+  if (status && contentType.includes(type)) {
     return res.json();
   } else {
     return Promise.reject(`Ошибка: ${res.status}`);
   }
 }
 
-  // {
-  //   _id: "60666c42cc7b410027a1a9b2",
-  //   name: "Флюоресцентная булка R2-D3",
-  //   type: "bun",
-  //   proteins: 44,
-  //   fat: 26,
-  //   carbohydrates: 85,
-  //   calories: 643,
-  //   price: 988,
-  //   image: "https://code.s3.yandex.net/react/code/bun-01.png",
-  //   image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-  //   image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-  //   __v: 0,
-  // }
-
 const dataPropTypes = PropTypes.shape({
   _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  proteins: PropTypes.number.isRequired,
+  fat: PropTypes.number.isRequired,
+  carbohydrates: PropTypes.number.isRequired,
+  calories: PropTypes.number.isRequired,
   price: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
+  image_large: PropTypes.string.isRequired,
 });
 
-export { API, checkResponse, dataPropTypes};
+export { API, checkResponse, dataPropTypes };

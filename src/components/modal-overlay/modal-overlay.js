048be2d099600de0da643e51d16ac9b2a6530478
Stern-Ritter <fadeew.a@mail.ru>
2022-01-24 14:46:55 +0300

    Add PropTypes to ModalOverlay component

--- src/components/modal-overlay/modal-overlay.js
+++ src/components/modal-overlay/modal-overlay.js
@@ -1,22 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styles from "./modal-overlay.module.css";
 
 function ModalOverlay(props) {
   const overlay = React.useRef(null);
 
   const closeModalOnClick = (evt) => {
-    if(evt.target === overlay.current) { props.closeHandler() }
-  }
+    if (evt.target === overlay.current) {
+      props.closeHandler();
+    }
+  };
 
   return (
-    <div
-      className={styles.overlay}
-      ref={overlay}
-      onClick={ closeModalOnClick }
-    >
+    <div className={styles.overlay} ref={overlay} onClick={closeModalOnClick}>
       {props.children}
     </div>
   );
+}
+
+ModalOverlay.propTypes = {
+  closeHandler: PropTypes.func.isRequired
 }
 
 export default ModalOverlay;

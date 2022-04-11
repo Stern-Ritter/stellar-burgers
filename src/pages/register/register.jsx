2965e57ad64e2d5e2e795541a09f79381609e999
Stern-Ritter <fadeew.a@mail.ru>
2022-04-11 23:30:13 +0300

    Add Register page

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
+import {
+  Input,
+  PasswordInput,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./register.module.css";
 
 function Register() {
   return (
-    <>
-      <Logo />
-    </>
+    <form className={styles.form}>
+      <h1 className="text text_type_main-medium mb-6">Регистрация</h1>
+      <Input type="text" placeholder="Имя" name="name" />
+      <Input type="email" placeholder="E-mail" name="email" />
+      <PasswordInput name="password" />
+      <Button type="primary" size="medium">
+        Зарегистрироваться
+      </Button>
+      <div className="mt-20 mb-4">
+        <span className="text text_type_main-default text_color_inactive mr-2">
+          Уже зарегистрированы?
+        </span>
+        <Link className={styles.link + " text text_type_main-default"}>
+          Войти
+        </Link>
+      </div>
+    </form>
   );
 }
 

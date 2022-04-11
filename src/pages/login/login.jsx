d0168240c7b806f234fe59a6dab2a5f7f29f8175
Stern-Ritter <fadeew.a@mail.ru>
2022-04-11 23:21:21 +0300

    Add Login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
-import styles from './login.module.css';
+import { Link } from "react-router-dom";
+import {
+  Input,
+  PasswordInput,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from "./login.module.css";
 
 function Login() {
   return (
-    <div>
-      <h1>Вход</h1>
-
-    </div>
+    <form className={styles.form}>
+      <h1 className="text text_type_main-medium mb-6">Вход</h1>
+      <Input type="email" placeholder="E-mail" name="email" />
+      <PasswordInput name="password" />
+      <Button type="primary" size="medium">
+        Войти
+      </Button>
+      <div className="mt-20 mb-4">
+        <span className="text text_type_main-default text_color_inactive mr-2">
+          Вы — новый пользователь?
+        </span>
+        <Link className={styles.link + " text text_type_main-default"}>
+          Зарегистрироваться
+        </Link>
+      </div>
+      <div>
+        <span className="text text_type_main-default text_color_inactive mr-2">
+          Забыли пароль?
+        </span>
+        <Link className={styles.link + " text text_type_main-default"}>
+          Восстановить пароль
+        </Link>
+      </div>
+    </form>
   );
 }
 

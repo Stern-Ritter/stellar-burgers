7c099560eefdb582f088e319cc123910b3baa20b
Stern-Ritter <fadeew.a@mail.ru>
2022-04-12 22:23:35 +0300

    Edit Register and Login pages actions

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,19 +12,6 @@ function checkResponse(res, type) {
   }
 }
 
-const dataPropTypes = PropTypes.shape({
-  _id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  proteins: PropTypes.number.isRequired,
-  fat: PropTypes.number.isRequired,
-  carbohydrates: PropTypes.number.isRequired,
-  calories: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired,
-  image_large: PropTypes.string.isRequired,
-});
-
 async function resetPasswordRequest(form) {
   const res = await fetch(`${API}/password-reset/reset`, {
     method: "POST",
@@ -49,7 +36,46 @@ async function updatePasswordRequest(form) {
   return data;
 }
 
+async function registerRequest(form) {
+  const res = await fetch(`${API}/auth/register`, {
+    method: "POST",
+    body: JSON.stringify(form),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await checkResponse(res, "application/json");
+  return data;
+}
+
+async function loginRequest(form) {
+  const res = await fetch(`${API}/auth/login`, {
+    method: "POST",
+    body: JSON.stringify(form),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await checkResponse(res, "application/json");
+  return data;
+}
+
+const dataPropTypes = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  proteins: PropTypes.number.isRequired,
+  fat: PropTypes.number.isRequired,
+  carbohydrates: PropTypes.number.isRequired,
+  calories: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  image_large: PropTypes.string.isRequired,
+});
+
 export {
+  registerRequest,
+  loginRequest,
   resetPasswordRequest,
   updatePasswordRequest,
   API,

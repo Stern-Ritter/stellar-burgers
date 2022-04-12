bfc6e0610e72512946cf02a79e8bdd31ac81d899
Stern-Ritter <fadeew.a@mail.ru>
2022-04-13 02:01:07 +0300

    Edit Profile page

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import ProfileNavigation from "../../components/profile-navigation/profile-navigation";
 import ProfileForm from "../../components/profile-form/profile-form";
+import { getUser } from '../../services/actions/user';
 import styles from "./profile.module.css";
 
 function Profile() {
   const dispatch = useDispatch();
   const { path } = useRouteMatch();
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
 
   return (
     <div className={styles.container}>
@@ -16,8 +21,8 @@ function Profile() {
           <ProfileNavigation />
           <ProfileForm />
         </Route>
-        <Route path={`${path}/orders`} exact></Route>
-        <Route path={`${path}/orders/:id`} exact></Route>
+        <Route path={`${path}/orders`} exact>{null}</Route>
+        <Route path={`${path}/orders/:id`} exact>{null}</Route>
       </Switch>
     </div>
   );

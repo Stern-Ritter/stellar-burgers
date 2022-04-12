44bf9208f2e8cbf4798fb60de8e33add0a71bfec
Stern-Ritter <fadeew.a@mail.ru>
2022-04-12 22:28:23 +0300

    Add cookies utils functions

---
+++ src/utils/cookies.js
@@ -0,0 +1,40 @@
+export function setCookie(name, value, props) {
+  props = props || {};
+  let expires = props.expires;
+
+  if (expires && typeof expires == "number") {
+    const date = new Date();
+    date.setTime(date.getTime() + expires * 1000);
+    expires = props.expires = date;
+  }
+
+  if (expires && expires.toUTCString) {
+    props.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+  let updatedCookie = name + "=" + value;
+  for (const propName in props) {
+    updatedCookie += "; " + propName;
+    const propValue = props[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+  document.cookie = updatedCookie;
+}
+
+export function getCookie(name) {
+  const matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+export function deleteCookie(name) {
+  setCookie(name, null, { expires: -1 });
+}

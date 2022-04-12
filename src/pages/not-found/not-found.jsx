a7cfe7d42f7612cde5f88860cfa642bff8b42e69
Stern-Ritter <fadeew.a@mail.ru>
2022-04-13 02:57:44 +0300

    Add basic NotFound page implementation

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import styles from "./not-found.module.css";
 
-function NotFound() {}
+function NotFound() {
+  return (
+    <div className={styles.container}>
+        <h1>Упс! Ошибка 404</h1>
+        <p>Страница, которую вы запрашиваете не существует</p>
+        <p>
+          проверьте адрес или перейдите на{" "}
+          <Link to="/" className={styles.link}>
+            Главную страницу
+          </Link>
+        </p>
+    </div>
+  );
+}
 
 export default NotFound;

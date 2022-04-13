4dbb8d1aa396a900fc037d0f992cfe6b209368b6
Stern-Ritter <fadeew.a@mail.ru>
2022-04-13 11:43:06 +0300

    Edit NotFound page

--- src/pages/not-found/not-found.jsx
+++ src/pages/not-found/not-found.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import styles from "./not-found.module.css";
 
 function NotFound() {
   return (
     <div className={styles.container}>
         <h1>Упс! Ошибка 404</h1>
-        <p>Страница, которую вы запрашиваете не существует</p>
+        <p>Страница, которую вы запрашиваете, не существует</p>
         <p>
           проверьте адрес или перейдите на{" "}
           <Link to="/" className={styles.link}>

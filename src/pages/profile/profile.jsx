682b50488d9078b3a4da9033909a350743abb7ef
Stern-Ritter <fadeew.a@mail.ru>
2022-04-17 15:13:42 +0300

    Add orders inner route to Profile page

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -2,26 +2,33 @@ import React from "react";
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import ProfileNavigation from "../../components/profile-navigation/profile-navigation";
 import ProfileForm from "../../components/profile-form/profile-form";
+import OrdersList from "../../components/orders-list/orders-list";
+import OrderInfo from "../../components/order-info/order-info";
 import styles from "./profile.module.css";
 
 function Profile() {
   const { path } = useRouteMatch();
 
   return (
-    <div className={styles.container}>
-      <Switch>
-        <Route path={path} exact>
+    <Switch>
+      <Route path={path} exact>
+        <div className={styles.container}>
           <ProfileNavigation />
           <ProfileForm />
-        </Route>
-        <Route path={`${path}/orders`} exact>
+        </div>
+      </Route>
+      <Route path={`${path}/orders`} exact>
+        <div className={styles.container}>
           <ProfileNavigation />
-        </Route>
-        <Route path={`${path}/orders/:id`} exact>
-          {null}
-        </Route>
-      </Switch>
-    </div>
+          <OrdersList type='enhanced'/>
+        </div>
+      </Route>
+      <Route path={`${path}/orders/:id`} exact>
+        <div className={styles["order-container"]}>
+          <OrderInfo />
+        </div>
+      </Route>
+    </Switch>
   );
 }
 

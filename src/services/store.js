a978d7dbc45402106bc4cb989a47b734f912ac5c
Stern-Ritter <fadeew.a@mail.ru>
2022-04-18 20:56:54 +0300

    Add socket middleware to store

--- src/services/store.js
+++ src/services/store.js
@@ -5,13 +5,13 @@ import { rootReducer } from "./reducers";
 import { socketMiddleware } from "./middleware/socketMiddleware";
 import { wsAllOrdersActions } from "./actions/all-orders";
 import { wsUserOrdersActions } from "./actions/user-orders";
-import { wsAPI } from "../utils/api";
+import { wsAllOrdersAPI, wsUserOrdersAPI } from "../utils/api";
 
 const enhancer = composeEnhancers(
   applyMiddleware(
     thunk,
-    socketMiddleware(`${wsAPI}/all`, wsAllOrdersActions),
-    socketMiddleware(wsAPI, wsUserOrdersActions)
+    socketMiddleware(wsAllOrdersAPI, wsAllOrdersActions),
+    socketMiddleware(wsUserOrdersAPI, wsUserOrdersActions)
   )
 );
 const store = createStore(rootReducer, enhancer);

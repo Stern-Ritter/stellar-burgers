42e277517f71cf95a7ef870ab7265f3edec46d5f
Stern-Ritter <fadeew.a@mail.ru>
2022-04-19 15:06:42 +0300

    Edit Ingredient page: remove modal window from page

--- src/pages/ingredient/ingredient.jsx
+++ src/pages/ingredient/ingredient.jsx
@@ -1,31 +1,23 @@
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { useHistory, useParams, useLocation } from "react-router-dom";
-import Modal from "../../components/modal/modal";
+import { useParams } from "react-router-dom";
 import IngredientDetails from "../../components/ingredient-details/ingredient-details";
+import PropTypes from "prop-types";
 import styles from "./ingredient.module.css";
 
-function Ingredient() {
-  const history = useHistory();
+function Ingredient({ type }) {
   const { id } = useParams();
-  const { state } = useLocation();
   const ingredientsData = useSelector((store) => store.ingredients.data);
 
   const selectedIngredient = useMemo(() => {
     return ingredientsData.find((el) => el._id === id);
   }, [ingredientsData, id]);
 
-  const closeHandler = () => {
-    history.goBack();
-  };
-
   return (
     <>
       {selectedIngredient &&
-        (state?.type === "modal" ? (
-          <Modal title="Детали ингредиента" closeHandler={closeHandler}>
-            <IngredientDetails ingredient={selectedIngredient} />
-          </Modal>
+        (type === "modal" ? (
+          <IngredientDetails ingredient={selectedIngredient} />
         ) : (
           <div className={styles.container + " pt-30"}>
             <h1 className={styles.title + " text text_type_main-large"}>
@@ -37,5 +29,9 @@ function Ingredient() {
     </>
   );
 }
+
+Ingredient.propTypes = {
+  type: PropTypes.string,
+};
 
 export default Ingredient;

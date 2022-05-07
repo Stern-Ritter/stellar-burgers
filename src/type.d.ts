5fe5d5c864b6fc353002173cde3d0281166849ea
Stern-Ritter <fadeew.a@mail.ru>
2022-05-07 21:29:38 +0300

    Added main types file

---
+++ src/type.d.ts
@@ -0,0 +1,54 @@
+declare module "*.module.css";
+
+type TIngredient = {
+  _id: string;
+  name: string;
+  type: string;
+  proteins: number;
+  fat: number;
+  carbohydrates: number;
+  calories: number;
+  price: number;
+  image: string;
+  image_large: string;
+};
+
+type TIngredients = {
+  bun: TIngredient;
+  main: Array<TIngredient>;
+};
+
+type TOrder = {
+  _id: string;
+  number: number;
+  name: string;
+  createdAt: string;
+  status: string;
+  ingredients: Array<string>;
+};
+
+type TResetPasswordForm = {
+  password: string;
+  token: string;
+};
+
+type TForgotPasswordForm = {
+  email: string;
+};
+
+type TRegisterForm = {
+  email: string;
+  password: string;
+  name: string;
+};
+
+type TLoginForm = {
+  email: string;
+  password: string;
+};
+
+type TUpdateUserForm = {
+  name: string;
+  email: string;
+  password: string;
+};
